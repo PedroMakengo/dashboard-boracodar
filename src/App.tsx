ab4636464a824c2fb5bc22e9afbf49fca9dce0a6
PedroMakengo <pedromakengomiguel@gmail.com>
2023-02-25 11:19:24 +0100

    Trabalhando

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import styles from './styles/styles.module.css'
-import { Card } from './components/Card'
+import { useState, useEffect } from 'react'
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
+import { Card } from './components/Card'
+import styles from './styles/styles.module.css'
+
 export function App() {
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    setInterval(() => {
+      setLoading(true)
+    }, 2000)
+  }, [])
+
   return (
     <div className={styles.app}>
       <div className={styles.container}>
         <div className={styles.contentHeader}>
-          {<Card title="NPS Geral" type="emoji" /> && (
+          {loading ? (
+            <Card title="NPS Geral" type="emoji" />
+          ) : (
             <SkeletonTheme baseColor="#363447" highlightColor="#292738">
-              <Skeleton count={3} height={100} />
+              <Skeleton count={18} height={10} />
             </SkeletonTheme>
           )}
-          <Card title="NPS Geral" type="emoji" />
-          <Card title="NPS Geral" type="emoji" />
+          {loading ? (
+            <Card title="NPS Geral" type="emoji" />
+          ) : (
+            <SkeletonTheme baseColor="#363447" highlightColor="#292738">
+              <Skeleton count={18} height={10} />
+            </SkeletonTheme>
+          )}
+          {loading ? (
+            <Card title="NPS Geral" type="emoji" />
+          ) : (
+            <SkeletonTheme baseColor="#363447" highlightColor="#292738">
+              <Skeleton count={18} height={10} />
+            </SkeletonTheme>
+          )}
         </div>
         <div className={styles.contentFooter}>
-          <Card title="NPS Geral" type="emoji" />
+          {loading ? (
+            <Card title="NPS Geral" type="emoji" />
+          ) : (
+            <SkeletonTheme baseColor="#363447" highlightColor="#292738">
+              <Skeleton count={8} height={10} />
+            </SkeletonTheme>
+          )}
         </div>
       </div>
     </div>
